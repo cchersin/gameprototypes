0eae7e3b3a050897d2e68a6d39444c7d5344def0
cchersin <Claudia@MacBook-Pro-di-Diego-Vatta.local>
2016-11-27 10:16:58 +0100

    levels

--- uberfranz/js/levels.js
+++ uberfranz/js/levels.js
@@ -4,8 +4,8 @@ var levels = [{
 	platforms:[{row:11,col:0, width:12, height:1},
 			   {row:0,col:0, width:1, height:11},
 			   {row:0,col:11, width:1, height:11},
-	           {row:3,col:8, width:3, height:1},
-			   {row:5,col:4, width:5, height:1, enemies : [{col:1},{col:3}]},
-			   {row:7,col:1, width:4, height:1, enemies : [{col:1}]},
-			   {row:9,col:5, width:6, height:1}]
+	           {row:3,col:1, width:3, height:1},
+			   {row:5,col:1, width:6, height:1, enemies : [{col:1},{col:3}]},
+			   {row:7,col:9, width:2, height:1},
+			   {row:9,col:5, width:6, height:1, enemies : [{col:1}]}]
 }]
