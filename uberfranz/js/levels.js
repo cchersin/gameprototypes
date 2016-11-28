9dbf7a86ccf8b9bb65a7241134b8216022206184
cchersin <Claudia@MacBook-Pro-di-Diego-Vatta.local>
2016-11-28 21:40:23 +0100

    jump

--- uberfranz/js/levels.js
+++ uberfranz/js/levels.js
@@ -1,11 +1,11 @@
 var levels = [{
-	container:{width:12,height:12},
+	container:{width:14,height:12},
 	mainCharacter:{row:10,col:10},
-	platforms:[{row:11,col:0, width:12, height:1},
+	platforms:[{row:11,col:0, width:14, height:1},
 			   {row:0,col:0, width:1, height:11},
-			   {row:0,col:11, width:1, height:11},
+			   {row:0,col:13, width:1, height:11},
 	           {row:3,col:1, width:3, height:1},
 			   {row:5,col:1, width:6, height:1, enemies : [{col:1},{col:3}]},
-			   {row:7,col:9, width:2, height:1},
-			   {row:9,col:5, width:6, height:1, enemies : [{col:1}]}]
+			   {row:6.8,col:9, width:4, height:1},
+			   {row:9,col:5, width:8, height:1, enemies : [{col:1}]}]
 }]
