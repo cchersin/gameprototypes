4e41415db6c1c129cfb49595131a3a2d47edfd6d
cchersin <Claudia@MacBook-Pro-di-Diego-Vatta.local>
2016-11-28 21:29:06 +0100

    costumes

--- uberfranz/js/levels.js
+++ uberfranz/js/levels.js
@@ -1,6 +1,6 @@
 var levels = [{
 	container:{width:12,height:12},
-	mainCharacter:{row:10,col:1},
+	mainCharacter:{row:10,col:10},
 	platforms:[{row:11,col:0, width:12, height:1},
 			   {row:0,col:0, width:1, height:11},
 			   {row:0,col:11, width:1, height:11},
