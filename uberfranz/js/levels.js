d2b2ab769a865e633d18bd496552478bf2fb60bd
cchersin <Claudia@MacBook-Pro-di-Diego-Vatta.local>
2016-12-07 20:41:00 +0100

    change level

--- uberfranz/js/levels.js
+++ uberfranz/js/levels.js
@@ -7,5 +7,23 @@ var levels = [{
 	           {row:3,col:1, width:3, height:1},
 			   {row:5,col:1, width:6, height:1, enemies : [{col:1},{col:3}]},
 			   {row:6.8,col:9, width:10, height:1},
-			   {row:9,col:5, width:14, height:1, enemies : [{col:1}]}]
-}]
+			   {row:9,col:5, width:14, height:1, enemies : [{col:1}]}]},
+   { container:{width:20,height:13},
+	 mainCharacter:{row:10,col:10},
+	 platforms:[{row:11,col:0, width:20, height:2},
+			   {row:0,col:0, width:1, height:11},
+			   {row:0,col:19, width:1, height:11},
+	           {row:3,col:1, width:3, height:1},
+			   {row:5,col:1, width:6, height:1, enemies : [{col:1},{col:3}]},
+			   {row:6.8,col:9, width:10, height:1},
+			   {row:9,col:5, width:14, height:1, enemies : [{col:1}]}]},
+	{ container:{width:20,height:13},
+	 mainCharacter:{row:10,col:10},
+	 platforms:[{row:11,col:0, width:20, height:2},
+			   {row:0,col:0, width:1, height:11},
+			   {row:0,col:19, width:1, height:11},
+	           {row:3,col:1, width:3, height:1},
+			   {row:5,col:1, width:6, height:1, enemies : [{col:1},{col:3}]},
+			   {row:6.8,col:9, width:10, height:1},
+			   {row:9,col:5, width:14, height:1, enemies : [{col:1}]}]}
+]
